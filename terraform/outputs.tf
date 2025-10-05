7356cc6a357ddd3c7e1a8c9434ff0e77c8ef49f0
Islam <islam.khaled@elmenus.com>
2025-10-06 01:09:13 +0300

    add ecr_repository_url output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,9 @@
+# ECR repository URL (used by GitHub Actions to push image)
+output "ecr_repository_url" {
+  description = "URL of the ECR repository for Docker pushes"
+  value       = aws_ecr_repository.app.repository_url
+}
+
 output "alb_dns_name" {
   description = "Public DNS name of the Application Load Balancer"
   value       = aws_lb.app_alb.dns_name
