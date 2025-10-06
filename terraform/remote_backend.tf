8d66866034f9ba9ff7b96e03998f638b53d39260
Islam <islam.khaled@elmenus.com>
2025-10-06 04:38:35 +0300

    configure new aws acc - alb support issue in old account

--- terraform/remote_backend.tf
+++ terraform/remote_backend.tf
@@ -1,12 +1,12 @@
 # Backend Infrastructure
 
-# S3 bucket for Terraform state 
+# S3 bucket for Terraform remote state
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "devops-trial-terraform-state"
+  bucket = "devops-trial-tf-backend-state"
 }
 
-# Enable versioning for recovery
-resource "aws_s3_bucket_versioning" "versioning" {
+# Enable versioning for Terraform state safety
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
   bucket = aws_s3_bucket.tf_state.id
 
   versioning_configuration {
@@ -14,26 +14,63 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
-# DynamoDB table for state locking 
+# DynamoDB table for Terraform state locking
 resource "aws_dynamodb_table" "tf_lock" {
-  name         = "devops-trial-terraform-locks"
+  name         = "devops-trial-tf-backend-locks"
   billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
     type = "S"
   }
-
-  hash_key = "LockID"
 }
-
 
 terraform {
   backend "s3" {
-    bucket         = "devops-trial-terraform-state"
-    key            = "ecs/terraform.tfstate"
+    bucket         = "devops-trial-tf-backend-state"
+    key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "devops-trial-terraform-locks"
+    dynamodb_table = "devops-trial-tf-backend-locks"
     encrypt        = true
   }
 }
+
+###islamkhaledaws@gmail.com configuration###
+# # S3 bucket for Terraform state 
+# resource "aws_s3_bucket" "tf_state" {
+#   bucket = "devops-trial-terraform-state"
+# }
+
+# # Enable versioning for recovery
+# resource "aws_s3_bucket_versioning" "versioning" {
+#   bucket = aws_s3_bucket.tf_state.id
+
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# # DynamoDB table for state locking 
+# resource "aws_dynamodb_table" "tf_lock" {
+#   name         = "devops-trial-terraform-locks"
+#   billing_mode = "PAY_PER_REQUEST"
+
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+
+#   hash_key = "LockID"
+# }
+
+
+# # terraform {
+# #   backend "s3" {
+# #     bucket         = "devops-trial-terraform-state"
+# #     key            = "ecs/terraform.tfstate"
+# #     region         = "us-east-1"
+# #     dynamodb_table = "devops-trial-terraform-locks"
+# #     encrypt        = true
+# #   }
+# # }
